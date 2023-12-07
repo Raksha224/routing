64a79e0441fb07c56f714d62d2c9b88df6ae90d2
Vivek9045 <gvivekkumar990@email.com>
2023-12-07 13:17:48 +0530

    Initial commit

---
+++ src/About.js
@@ -0,0 +1,13 @@
+// File: About.jsx
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ backgroundColor: 'lightgreen', padding: '20px' }}>
+      <h2>About</h2>
+      <p>This is the about page.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+// File: Contact.jsx
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div style={{ backgroundColor: 'lightcoral', padding: '20px' }}> 
+      <h2>Contact</h2>
+      <p>Feel free to contact us.</p>
+    </div>
+  );
+};
+
+export default Contact;
